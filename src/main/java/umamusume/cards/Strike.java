5cc03ecc4631b168435d0ec9d185e9fd0d76fc95
nian <nianruoshui@gmail.com>
2025-05-11 21:47:55 +0800

    first

---
+++ src/main/java/umamusume/cards/Strike.java
@@ -0,0 +1,39 @@
+package umamusume.cards;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Strike extends CustomCard {
+
+    public static final String ID = "UmaMod:Strike";
+    private static final String NAME = "打击";
+    private static final String IMG_PATH = "umaResources/img/cards/strike.png";
+    private static final int COST = 1;
+    private static final String DESCRIPTION = "造成 !D! 点伤害";
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardColor COLOR = CardColor.COLORLESS;
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget Target = CardTarget.ENEMY;
+
+    public Strike(){
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, Target);
+        this.damage = this.baseDamage = 6;
+        this.tags.add(CardTags.STARTER_STRIKE);
+        this.tags.add(CardTags.STRIKE);
+    }
+    public void upgrade(){
+        if(!this.upgraded){
+            this.upgradeName();
+            this.upgradeDamage(3);
+        }
+    }
+    public void use(AbstractPlayer p, AbstractMonster m){
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m,new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL))
+        );
+    }
+}
---
+++ src/main/java/umamusume/modcore/MainMod.java
@@ -0,0 +1,24 @@
+package umamusume.modcore; //当前java文件被引用时的名字
+import basemod.BaseMod;    //导入
+import basemod.interfaces.EditCardsSubscriber;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import umamusume.cards.Strike;
+
+@SpireInitializer //语法糖 总之就是加载mod
+public class MainMod implements EditCardsSubscriber {
+    //建立一个公共public类class，叫MainMod，要求实现EditCardsSubscriber所定义的方法
+
+    //类MainMod的构造方法，也就是这个类被创建时会被自动调用
+    public MainMod(){
+        BaseMod.subscribe(this);//通知BaseMod，要求把我们自己(this)进行注册
+    }
+    //初始化
+    public static void initialize(){
+
+        new MainMod();
+    }
+    //编辑卡牌的代码
+    public void receiveEditCards(){
+        BaseMod.addCard(new Strike());
+    }
+}
