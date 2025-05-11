51f7879e513b421209f496cd246ebbb1584b989a
nian <nianruoshui@gmail.com>
2025-05-12 06:32:44 +0800

    加入本地化

--- src/main/java/umamusume/cards/Strike.java
+++ src/main/java/umamusume/cards/Strike.java
@@ -4,16 +4,21 @@ import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class Strike extends CustomCard {
 
     public static final String ID = "UmaMod:Strike";
-    private static final String NAME = "打击";
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String NAME = CARD_STRINGS.NAME;
+//    private static final String NAME = "打击";
     private static final String IMG_PATH = "umaResources/img/cards/strike.png";
     private static final int COST = 1;
-    private static final String DESCRIPTION = "造成 !D! 点伤害";
+    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+//    private static final String DESCRIPTION = "造成 !D! 点伤害";
     private static final CardType TYPE = CardType.ATTACK;
     private static final CardColor COLOR = CardColor.COLORLESS;
     private static final CardRarity RARITY = CardRarity.BASIC;
@@ -29,6 +34,8 @@ public class Strike extends CustomCard {
         if(!this.upgraded){
             this.upgradeName();
             this.upgradeDamage(3);
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
     public void use(AbstractPlayer p, AbstractMonster m){
--- src/main/java/umamusume/modcore/MainMod.java
+++ src/main/java/umamusume/modcore/MainMod.java
@@ -1,11 +1,14 @@
 package umamusume.modcore; //当前java文件被引用时的名字
 import basemod.BaseMod;    //导入
 import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import umamusume.cards.Strike;
 
 @SpireInitializer //语法糖 总之就是加载mod
-public class MainMod implements EditCardsSubscriber {
+public class MainMod implements EditCardsSubscriber, EditStringsSubscriber {
     //建立一个公共public类class，叫MainMod，要求实现EditCardsSubscriber所定义的方法
 
     //类MainMod的构造方法，也就是这个类被创建时会被自动调用
@@ -20,6 +23,16 @@ public class MainMod implements EditCardsSubscriber {
     //编辑卡牌的代码
     public void receiveEditCards(){
         BaseMod.addCard(new Strike());
+    }
+
+    public void receiveEditStrings(){
+        String lang;
+        if (Settings.language == Settings.GameLanguage.ZHS){
+            lang = "ZHS";
+        }else{
+            lang = "ENG";
+        }
+        BaseMod.loadCustomStringsFile(CardStrings.class, "resources/localization/" + lang + "/cards.json");
     }
 }
 
