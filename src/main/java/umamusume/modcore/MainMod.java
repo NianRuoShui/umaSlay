ed744836eb5c76a16e22111f9635066fc2ba01cc
Fitswc <ericchufengzeng@163.com>
2025-05-12 19:25:29 +0800

    Gather Resource reference to ResourceManager

--- src/main/java/umamusume/modcore/MainMod.java
+++ src/main/java/umamusume/modcore/MainMod.java
@@ -12,44 +12,53 @@ import com.megacrit.cardcrawl.localization.CharacterStrings;
 import umamusume.cards.Defend;
 import umamusume.cards.Strike;
 import umamusume.characters.uma;
-import com.badlogic.gdx.graphics.Color;
+import umamusume.modcore.ResourceManager;
 import static umamusume.characters.uma.PlayerColorEnum.uma_blue;
 import static umamusume.characters.uma.PlayerColorEnum.UMA_CHARACTER;
 
 @SpireInitializer //语法糖 总之就是加载mod
 public class MainMod implements EditCardsSubscriber, EditStringsSubscriber, EditCharactersSubscriber {
     //建立一个公共public类class，叫MainMod，要求实现EditCardsSubscriber所定义的方法
-    // 人物选择界面按钮的图片
-    private static final String MY_CHARACTER_BUTTON = "umaResources/img/cards/strike.png";
-    // 人物选择界面的立绘
-    private static final String MY_CHARACTER_PORTRAIT = "umaResources/img/cards/strike.png";
-    // 攻击牌的背景（小尺寸）
-    private static final String BG_ATTACK_512 = "umaResources/img/cards/strike.png";
-    // 能力牌的背景（小尺寸）
-    private static final String BG_POWER_512 = "umaResources/img/cards/strike.png";
-    // 技能牌的背景（小尺寸）
-    private static final String BG_SKILL_512 = "umaResources/img/cards/strike.png";
-    // 在卡牌和遗物描述中的能量图标
-    private static final String SMALL_ORB = "umaResources/img/cards/strike.png";
-    // 攻击牌的背景（大尺寸）
-    private static final String BG_ATTACK_1024 = "umaResources/img/cards/strike.png";
-    // 能力牌的背景（大尺寸）
-    private static final String BG_POWER_1024 = "umaResources/img/cards/strike.png";
-    // 技能牌的背景（大尺寸）
-    private static final String BG_SKILL_1024 = "umaResources/img/cards/strike.png";
-    // 在卡牌预览界面的能量图标
-    private static final String BIG_ORB = "umaResources/img/cards/strike.png";
-    // 小尺寸的能量图标（战斗中，牌堆预览）
-    private static final String ENEYGY_ORB = "umaResources/img/cards/strike.png";
-    // 颜色
-    public static final Color MY_COLOR = new Color(129.0F / 255.0F, 232.0F / 255.0F, 9.0F / 225.0F, 1.0F);
+
 
     //类MainMod的构造方法，也就是这个类被创建时会被自动调用
     public MainMod() {
         BaseMod.subscribe(this);//通知BaseMod，要求把我们自己(this)进行注册
-        BaseMod.addColor(uma_blue, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR, MY_COLOR, BG_ATTACK_512, BG_SKILL_512, BG_POWER_512, ENEYGY_ORB, BG_ATTACK_1024, BG_SKILL_1024, BG_POWER_1024, BIG_ORB, SMALL_ORB);
+        BaseMod.addColor(uma_blue,
+                ResourceManager.MY_COLOR.cpy(),
+                ResourceManager.MY_COLOR.cpy(),
+                ResourceManager.MY_COLOR.cpy(),
+                ResourceManager.MY_COLOR.cpy(),
+                ResourceManager.MY_COLOR.cpy(),
+                ResourceManager.MY_COLOR.cpy(),
+                ResourceManager.MY_COLOR.cpy(),
+                ResourceManager.BG_ATTACK_512,
+                ResourceManager.BG_SKILL_512,
+                ResourceManager.BG_POWER_512,
+                ResourceManager.ENEYGY_ORB,
+                ResourceManager.BG_ATTACK_1024,
+                ResourceManager.BG_SKILL_1024,
+                ResourceManager.BG_POWER_1024,
+                ResourceManager.BIG_ORB,
+                ResourceManager.SMALL_ORB);
 
-        //addColor(AbstractCard.CardColor color, Color bgColor, Color backColor, Color frameColor, Color frameOutlineColor, Color descBoxColor, Color trailVfxColor, Color glowColor, String attackBg, String skillBg, String powerBg, String energyOrb, String attackBgPortrait, String skillBgPortrait, String powerBgPortrait, String energyOrbPortrait, String cardEnergyOrb)
+        //addColor(AbstractCard.CardColor color,
+        // Color bgColor,
+        // Color backColor,
+        // Color frameColor,
+        // Color frameOutlineColor,
+        // Color descBoxColor,
+        // Color trailVfxColor,
+        // Color glowColor,
+        // String attackBg,
+        // String skillBg,
+        // String powerBg,
+        // String energyOrb,
+        // String attackBgPortrait,
+        // String skillBgPortrait,
+        // String powerBgPortrait,
+        // String energyOrbPortrait,
+        // String cardEnergyOrb)
     }
 
     //初始化
@@ -68,8 +77,8 @@ public class MainMod implements EditCardsSubscriber, EditStringsSubscriber, Edit
         if (Settings.language == Settings.GameLanguage.ZHS) {
             lang = "ZHS";
         } else {
-            lang = "ZHS";
-            // lang = "ENG";
+//            lang = "ZHS";
+             lang = "ENG";
         }
         BaseMod.loadCustomStringsFile(CardStrings.class, "umaResources/localization/" + lang + "/cards.json");
         BaseMod.loadCustomStringsFile(CharacterStrings.class, "umaResources/localization/" + lang + "/characters.json");
@@ -77,7 +86,11 @@ public class MainMod implements EditCardsSubscriber, EditStringsSubscriber, Edit
 
     public void receiveEditCharacters() {
         // 向basemod注册人物
-        BaseMod.addCharacter(new uma(CardCrawlGame.playerName), MY_CHARACTER_BUTTON, MY_CHARACTER_PORTRAIT, UMA_CHARACTER);
+        BaseMod.addCharacter(new uma(CardCrawlGame.playerName),
+                ResourceManager.MY_CHARACTER_BUTTON,
+                ResourceManager.MY_CHARACTER_PORTRAIT,
+                UMA_CHARACTER
+        );
     }
 
 }
---
+++ src/main/java/umamusume/modcore/ResourceManager.java
@@ -0,0 +1,36 @@
+package umamusume.modcore;
+
+import com.badlogic.gdx.graphics.Color;
+//import com.megacrit.cardcrawl.helpers.CardHelper;
+
+public class ResourceManager {
+    // 人物选择界面按钮的图片
+    public static final String MY_CHARACTER_BUTTON = "umaResources/img/cards/strike.png";
+    // 人物选择界面的立绘
+    public static final String MY_CHARACTER_PORTRAIT = "umaResources/img/cards/strike.png";
+    // 攻击牌的背景（小尺寸）
+    public static final String BG_ATTACK_512 = "umaResources/img/cards/strike.png";
+    // 能力牌的背景（小尺寸）
+    public static final String BG_POWER_512 = "umaResources/img/cards/strike.png";
+    // 技能牌的背景（小尺寸）
+    public static final String BG_SKILL_512 = "umaResources/img/cards/strike.png";
+    // 在卡牌和遗物描述中的能量图标
+    public static final String SMALL_ORB = "umaResources/img/cards/strike.png";
+    // 攻击牌的背景（大尺寸）
+    public static final String BG_ATTACK_1024 = "umaResources/img/cards/strike.png";
+    // 能力牌的背景（大尺寸）
+    public static final String BG_POWER_1024 = "umaResources/img/cards/strike.png";
+    // 技能牌的背景（大尺寸）
+    public static final String BG_SKILL_1024 = "umaResources/img/cards/strike.png";
+    // 在卡牌预览界面的能量图标
+    public static final String BIG_ORB = "umaResources/img/cards/strike.png";
+    // 小尺寸的能量图标（战斗中，牌堆预览）
+    public static final String ENEYGY_ORB = "umaResources/img/cards/strike.png";
+    // 颜色
+
+    //
+    public static final Color MY_COLOR = new Color(129.0F / 255.0F, 232.0F / 255.0F, 9.0F / 225.0F, 1.0F);
+    // Actually these two method has same function
+//    public static final Color UMA_COLOR = CardHelper.getColor(129.0F, 232.0F, 9.0F);
+    //
+}
