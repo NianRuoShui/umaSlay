2555dcc1b5480ba09613e21e512554acdb1f2047
Nian <nianruoshui@gmail.com>
2025-05-14 16:39:49 +0800

    delete import

--- src/main/java/umamusume/powers/umaPower.java
+++ src/main/java/umamusume/powers/umaPower.java
@@ -12,7 +12,6 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import static umamusume.characters.Oguri.PlauerTagsEnum.Uma_Oguri_food;
 
